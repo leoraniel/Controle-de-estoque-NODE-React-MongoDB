1417aa5c7c557fb21634725f188b9a7cee9cff6b
leoraniel <leo.raniel16@gmail.com>
2021-11-28 14:35:24 -0300

    Add schema for database

--- src/app/models/products.js
+++ src/app/models/products.js
@@ -2,10 +2,11 @@ const mongoose = require('../../database/index')
 
 const productsSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    amount: {type: Number, required:true},
+    amount: {type: Number, required: true},
     createdAt: {type: Date, default: Date.now}
 })
 
-const product = mongoose.Model('product', productsSchema)
+const product = mongoose.model('product', productsSchema)
 
-module.exports = products
+
+module.exports = product
